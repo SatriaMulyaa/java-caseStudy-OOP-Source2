4d04c1b195e1b128d53afbf2f048a0a9e686df81
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-06-04 13:06:51 +0700

    make entity-done

--- src/entity/Todolist.java
+++ src/entity/Todolist.java
@@ -1,4 +1,22 @@
 package entity;
 
 public class Todolist {
+    private String todo;
+
+    // constructor
+    public Todolist(){}
+
+    public Todolist(String todo){
+        this.todo = todo;
+    }
+
+    // getter
+    public String getTodo(){
+        return todo;
+    }
+
+    // setter
+    public void setTodo(String todo){
+        this.todo = todo;
+    }
 }
