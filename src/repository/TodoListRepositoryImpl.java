466f633352414a4b1bb30694c0accea5f5f0bfb4
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-06-05 16:49:27 +0700

    Repository and service, add todolist

--- src/repository/TodoListRepositoryImpl.java
+++ src/repository/TodoListRepositoryImpl.java
@@ -11,9 +11,39 @@ public class TodoListRepositoryImpl implements TodoListRepository {
         return data;
     }
 
+    public boolean isFull(){
+     var isFull = true;
+     for (int i = 0; i <data.length ; i++) {
+      if(data[i] == null){
+       isFull = false;
+       break;
+      }
+     }
+     return isFull;
+    }
+
+    public void resizeIsFull(){
+     // if full we resize the size to be 2X
+     if(isFull()){
+      var temp = data;
+      data = new Todolist[data.length * 2];
+
+      for (int i = 0; i < temp.length ; i++) {
+       data[i] = temp[i];
+      }
+     }
+    }
+
     @Override
     public void add(Todolist todolist) {
-
+     resizeIsFull();
+     // add in position data Array null
+     for (int i = 0; i < data.length ; i++) {
+      if(data[i] == null){
+       data[i] = todolist;
+       break;
+      }
+     }
     }
 
     @Override
